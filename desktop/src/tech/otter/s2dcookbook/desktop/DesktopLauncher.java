165148a87957c0fa56d96f702f905a55420cadac
John Lynn <john@silferein.com>
2016-10-08 13:07:02 -0700

    Initialize libGDX repository.

---
+++ desktop/src/tech/otter/s2dcookbook/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package tech.otter.s2dcookbook.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import tech.otter.s2dcookbook.Scene2DCookBook;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Scene2DCookBook(), config);
+	}
+}
---
+++ html/src/tech/otter/s2dcookbook/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package tech.otter.s2dcookbook.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import tech.otter.s2dcookbook.Scene2DCookBook;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Scene2DCookBook();
+        }
+}
